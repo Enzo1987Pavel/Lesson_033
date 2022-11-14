17dcdd4008663b7d7ff718f4108b1394243f0c28
Enzo1987Pavel <Enzo1987@mail.ru>
2022-11-14 21:14:36 +0300

    Add .exv_example file

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -12,20 +12,29 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+
+import environ
+
+env = environ.Env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+ENV_FILE_PATH = BASE_DIR.parent.joinpath(".env")
+environ.Env.read_env(ENV_FILE_PATH)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*+=l*$6wt)h62=v(7grrcbhj83nkbi0gdf!3+y%4xg1-wigqd3'
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
