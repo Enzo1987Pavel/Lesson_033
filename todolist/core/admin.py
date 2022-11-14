43b730c875d1f8a066304552200ccb01d6da2f01
Enzo1987Pavel <Enzo1987@mail.ru>
2022-11-14 23:35:11 +0300

    Add superuser

--- todolist/core/admin.py
+++ todolist/core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ("username", "first_name", "last_name", "email")  # Отображение полей в Админке
+    search_fields = ("username", "first_name", "last_name", "email")  # Поиск по полям в Админке
+    list_filter = ("is_staff", "is_active", "is_superuser")  # Фильтры по полям в Админке
+
+
+class PostCodesAdmin(admin.ModelAdmin):
+    exclude = ("password",)  # Скрываем поле "Пароль" в Админке
