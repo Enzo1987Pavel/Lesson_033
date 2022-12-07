3f5bea0f01232020a42f91f0f9b0f201b4117907
Enzo1987Pavel <Enzo1987@mail.ru>
2022-12-07 14:21:53 +0300

    Add Goals, Categories and Comments (models, view, serializers)

--- todolist/goals/admin.py
+++ todolist/goals/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from goals.models import GoalCategory
+
+admin.site.site_header = "Модуль 7 SkyPro"
+admin.site.site_title = "Диплом"
+admin.site.index_title = "Редактирование целей"
+
+
+class GoalCategoryAdmin(admin.ModelAdmin):
+    list_display = ("title", "user", "created", "updated")
+    search_fields = ("title", "user")
+
+
+admin.site.register(GoalCategory, GoalCategoryAdmin)
