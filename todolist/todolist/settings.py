0f94532ebf154d621b7eef168c042ff4d81585c6
Enzo1987Pavel <Enzo1987@mail.ru>
2022-12-12 21:56:51 +0300

    Add "Bot" app

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -172,3 +172,6 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
 }
+
+
+TG_TOKEN = env.str("BOT_TOKEN")
