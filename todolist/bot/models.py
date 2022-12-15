d8bd7bc85432510bb4352db6559434ddcfd804a5
Enzo1987Pavel <Enzo1987@mail.ru>
2022-12-15 21:12:20 +0300

    Edit bot, change models

--- todolist/bot/models.py
+++ todolist/bot/models.py
@@ -15,10 +15,10 @@ class TgUser(models.Model):
     tg_user_id = models.BigIntegerField(verbose_name="tg_user_id", unique=True)
     username = models.CharField(max_length=150, verbose_name="tg_username", null=True, blank=True, default=None)
     user = models.ForeignKey(User, models.PROTECT, null=True, blank=True, default=None, verbose_name="Пользователь")
-    verification_code = models.CharField(max_length=12, unique=True, verbose_name="Код подтверждения")
+    verification_code = models.CharField(max_length=8, unique=True, verbose_name="Код подтверждения")
 
     def set_verification_code(self) -> None:
-        length = 12
+        length = 8
         digits = string.digits
         v_code = "".join(random.sample(digits, length))
         self.verification_code = v_code
