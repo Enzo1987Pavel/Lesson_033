01e3f3298eb00688f14e6b5cbfac904a80201006
Enzo1987Pavel <Enzo1987@mail.ru>
2022-12-22 02:00:05 +0300

    Edit Views and Serializers for BOT

--- todolist/bot/views.py
+++ todolist/bot/views.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from rest_framework import generics, status
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
@@ -10,10 +11,17 @@ from .tg.client import TgClient
 
 class BotVerificationView(generics.UpdateAPIView):
     model = TgUser
-
     permission_classess = [IsAuthenticated]
     serializer_class = TgUserSerializer
     http_method_names = ["patch"]
+
+    # def get_object(self):
+    #     try:
+    #         obj = self.model.objects.get(verification_code=self.request.data.get("verification_code"))
+    #     except self.model.DoesNotExist:
+    #         raise ValidationError("Введен неверный код верификации!")
+    #
+    #     return obj
 
     def patch(self, request, *args, **kwargs):
         data = self.serializer_class(request.data).data
@@ -21,9 +29,9 @@ class BotVerificationView(generics.UpdateAPIView):
         tg_user = TgUser.objects.filter(verification_code=data["verification_code"]).first()
 
         if not tg_user:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+            raise ValidationError("Введен неверный код верификации!")
 
-        tg_user.user = request.user
+        tg_user = request.user
         tg_user.save()
-        tg_client.send_message(chat_id=tg_user.tg_chat_id, text="Регистрация прошла успешно!")
+        tg_client.send_message(chat_id=tg_user.tg_chat_id, text="Good!")
         return Response(data=data, status=status.HTTP_200_OK)
