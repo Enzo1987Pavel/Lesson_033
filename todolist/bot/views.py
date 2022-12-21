04ec2d59eccfa3e0a835c5dbd90e422ff1e7bd8d
Enzo1987Pavel <Enzo1987@mail.ru>
2022-12-21 21:50:19 +0300

    Edit Requirements

--- todolist/bot/views.py
+++ todolist/bot/views.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from rest_framework import generics, status
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
@@ -14,13 +15,21 @@ class BotVerificationView(generics.UpdateAPIView):
     serializer_class = TgUserSerializer
     http_method_names = ["patch"]
 
+    def get_object(self):
+        try:
+            obj = self.model.objects.get(verification_code=self.request.data.get("verification_code"))
+        except self.model.DoesNotExist:
+            raise ValidationError("Введен неверный код верификации!")
+
+        return obj
+
     def patch(self, request, *args, **kwargs):
         data = self.serializer_class(request.data).data
         tg_client = TgClient(settings.BOT_TOKEN)
         tg_user = TgUser.objects.filter(verification_code=data["verification_code"]).first()
 
         if not tg_user:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+            raise ValidationError({"verification_code": "Неправильный верификационный код"})
 
         tg_user = request.user
         tg_user.save()
