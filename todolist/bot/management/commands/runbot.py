3d478fb5486dcd31f07d1c8bd75cbf7ec5e47a67
Enzo1987Pavel <Enzo1987@mail.ru>
2022-12-21 23:47:00 +0300

    Edit Views and Serializers for BOT

--- todolist/bot/management/commands/runbot.py
+++ todolist/bot/management/commands/runbot.py
@@ -10,12 +10,6 @@ class Command(BaseCommand):
     help = "Runs Telegram bot"
     tg_client = TgClient(settings.BOT_TOKEN)
 
-    # def handle_unverified_user(self, msg: Message, tg_user: TgUser):
-    #     code = "123"
-    #     tg_user.verification_code = code
-    #     tg_user.save()
-    #     self.tg_client.send_message(chat_id=msg.chat.id, text=f"{code}")
-
     def handle_user(self, msg: Message):
         tg_user, created = TgUser.objects.get_or_create(
             tg_user_id=msg.msg_from.id,
