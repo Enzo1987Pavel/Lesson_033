96aa96e98bdd0e183ce75744752fc413ce3dfb82
Enzo1987Pavel <Enzo1987@mail.ru>
2022-12-22 03:28:06 +0300

    Edit Views and Serializers for BOT

--- todolist/bot/serializers.py
+++ todolist/bot/serializers.py
@@ -6,4 +6,6 @@ from bot.models import TgUser
 class TgUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = TgUser
+        read_only_fields = ("tg_id", "tg_user_id")
         fields = ("verification_code",)
+
