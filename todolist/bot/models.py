73addd62df21982876228173e2981cc12262aea2
Enzo1987Pavel <Enzo1987@mail.ru>
2022-12-22 06:30:20 +0300

    Edit View for BOT

--- todolist/bot/models.py
+++ todolist/bot/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.core.validators import MinLengthValidator
 from django.utils.crypto import get_random_string
 
 
@@ -10,7 +9,6 @@ class TgUser(models.Model):
 
     tg_chat_id = models.BigIntegerField(verbose_name="id чата")
     tg_user_id = models.BigIntegerField(unique=True, verbose_name="id пользователя")
-    # tg_username = models.CharField(max_length=32, validators=[MinLengthValidator(5)], verbose_name="Имя пользователя")
     user = models.ForeignKey("core.User", null=True, blank=True, on_delete=models.CASCADE, verbose_name="Пользователь приложения")
     verification_code = models.CharField(max_length=15, unique=True, verbose_name="Код верификации")
 
