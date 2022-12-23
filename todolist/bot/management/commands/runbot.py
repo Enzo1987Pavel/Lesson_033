d7e146c48d1ffa3baaa520b3f2a11ace0c71f24d
Enzo1987Pavel <Enzo1987@mail.ru>
2022-12-23 22:15:02 +0300

    New commands for bot

--- todolist/bot/management/commands/runbot.py
+++ todolist/bot/management/commands/runbot.py
@@ -4,11 +4,48 @@ from django.core.management.base import BaseCommand
 from bot.models import TgUser
 from bot.tg.client import TgClient
 from bot.tg.dc import Message
+from goals.models import Goal, GoalCategory
 
 
 class Command(BaseCommand):
     help = "Runs Telegram bot"
     tg_client = TgClient(settings.BOT_TOKEN)
+
+    def choose_categories(self, msg: Message, tg_user: TgUser):
+        pass
+
+    def get_categories(self, msg: Message, tg_user: TgUser):
+        goal_categories = GoalCategory.objects.filter(
+            board__participants__user=tg_user.user,
+            is_deleted=False,
+        )
+        goal_categories_srt = "\n".join(["🔹 " + goal.title for goal in goal_categories])
+
+        self.tg_client.send_message(
+            chat_id=msg.chat.id,
+            text=f"🏷 Выберите категорию:\n"
+                 f"====================\n"
+                 f"{goal_categories_srt}:\n"
+                 f"====================\n"
+        )
+
+    def create_goal(self):
+        self.get_categories()
+        self.choose_categories()
+        #create goal
+
+    def get_goal(self, msg: Message, tg_user: TgUser):
+        goals = Goal.objects.filter(category__board__participants__user=tg_user.user).exclude(
+            status=Goal.Status.archived)
+        goals_str = "\n".join(["🔹 " + goal.title for goal in goals])
+
+        self.tg_client.send_message(
+            chat_id=msg.chat.id,
+            text=f"📌 Ваш список целей:\n"
+                 f"==================\n"
+                 f"{goals_str}:\n"
+                 f"==================\n"
+        )
 
     def handle_user(self, msg: Message):
         tg_user, created = TgUser.objects.get_or_create(
@@ -26,11 +63,22 @@ class Command(BaseCommand):
                      f"{tg_user.verification_code}\n\n"
                      f"на сайте pesaulov87.ga"
             )
+        elif msg.text == "/goals":
+            self.get_goal(msg, tg_user)
+
+        elif msg.text == "/create":
+
         else:
             self.tg_client.send_message(
                 chat_id=msg.chat.id,
-                text="Ваш аккаунт уже был подтвержден!"
+                text=f"⛔ Вы ввели неизвестную команду ( **{msg.text}** )!"
             )
+
+        # else:
+        #     self.tg_client.send_message(
+        #         chat_id=msg.chat.id,
+        #         text="Ваш аккаунт уже был подтвержден!"
+        #     )
 
     def handle(self, *args, **options):
         offset = 0
