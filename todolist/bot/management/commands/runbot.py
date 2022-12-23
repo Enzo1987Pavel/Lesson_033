f017dcbc96bb479db019d1f7fd46afc5b24afdd4
Enzo1987Pavel <Enzo1987@mail.ru>
2022-12-23 20:45:59 +0300

    WORKING BOT with verification code

--- todolist/bot/management/commands/runbot.py
+++ todolist/bot/management/commands/runbot.py
@@ -10,17 +10,11 @@ class Command(BaseCommand):
     help = "Runs Telegram bot"
     tg_client = TgClient(settings.BOT_TOKEN)
 
-    # def handle_user_without_verification(self, msg: Message, tg_user: TgUser):
-    #     tg_user.generate_verification_code()
-    #     tg_user.save(update_fields=["verification_code"])
-    #     self.tg_client.send_message(
-    #         msg.chat.id, f"[verification code] {tg_user.verification_code}"
-    #     )
-
     def handle_user(self, msg: Message):
         tg_user, created = TgUser.objects.get_or_create(
             tg_user_id=msg.msg_from.id,
             tg_chat_id=msg.chat.id,
+
         )
 
         if created:
@@ -32,10 +26,10 @@ class Command(BaseCommand):
                      f"{tg_user.verification_code}\n\n"
                      f"на сайте pesaulov87.ga"
             )
-        elif not created:
+        else:
             self.tg_client.send_message(
                 chat_id=msg.chat.id,
-                text="Вы уже активировали аккаунт!"
+                text="Ваш аккаунт уже активирован!"
             )
 
     def handle(self, *args, **options):
