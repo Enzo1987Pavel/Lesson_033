7a4c1e688285cc3685681c95f652f5dbd90fcd48
Enzo1987Pavel <Enzo1987@mail.ru>
2022-12-26 15:06:35 +0300

    Bot commit #2

--- todolist/bot/serializers.py
+++ todolist/bot/serializers.py
@@ -1,10 +1,22 @@
-# from rest_framework import serializers
-#
-# from bot.models import TgUser
-#
-#
-# class TgUserSerializer(serializers.ModelSerializer):
-#
-#     class Meta:
-#         model = TgUser
-#         fields = ("verification_code",)
+from django.conf import settings
+from rest_framework import serializers
+
+from bot.models import TgUser
+from bot.tg.client import TgClient
+
+
+class BotVerifyCodeUpdateView(serializers.ModelSerializer):
+    user_id = serializers.CurrentUserDefault
+    tg_id = serializers.IntegerField(source="tg_user_id")
+    username = serializers.CharField(source="tg_username")
+
+    class Meta:
+        model = TgUser
+        fields = ["tg_id", "username", "verification_code", "user_id"]
+
+    def update(self, instance, validated_data):
+        instance.user = self.context["request"].user
+        instance.save()
+        TgClient(token=settings.BOT_TOKEN).send_message(chat_id=instance.tg_chat_id,
+                                                        text="Верификация прошла успешно")
+        return instance
--- todolist/bot/views.py
+++ todolist/bot/views.py
@@ -1,36 +1,42 @@
-from django.conf import settings
-from rest_framework import generics, status
-from rest_framework.exceptions import ValidationError
+from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 
 from bot.models import TgUser
 
-# from .serializers import TgUserSerializer
-from .tg.client import TgClient
+from .serializers import BotVerifyCodeUpdateView
 
 
 class BotVerificationView(generics.UpdateAPIView):
     model = TgUser
-    permission_classess = [IsAuthenticated]
-    # serializer_class = TgUserSerializer
+    serializer_class = BotVerifyCodeUpdateView
     http_method_names = ["patch"]
+    permission_classes = [IsAuthenticated]
 
-    queryset = TgUser.objects.all()
+    def get_object(self):
+        return get_object_or_404(TgUser, verification_code=self.request.data["verification_code"])
 
-    def patch(self, request, *args, **kwargs):
-        verif_code = self.request.data.get("verification_code")
 
-        if not verif_code:
-            raise ValidationError({"Указан неверный код проверки!"})
-
-        tg_client = TgClient(settings.BOT_TOKEN)
-        try:
-            tg_user = TgUser.objects.get(verification_code=verif_code)
-        except self.model.DoesNotExist:
-            raise ValidationError({"Не существует пользователя с таким кодом!"})
-
-        tg_user.user = self.request.user
-        tg_user.save()
-        tg_client.send_message(chat_id=tg_user.tg_chat_id, text=f"✅ Аккаунт подтвержден!!!")
-        return Response(data=verif_code, status=status.HTTP_201_CREATED)
+    # model = TgUser
+    # permission_classess = [IsAuthenticated]
+    # # serializer_class = TgUserSerializer
+    # http_method_names = ["patch"]
+    #
+    # queryset = TgUser.objects.all()
+    #
+    # def patch(self, request, *args, **kwargs):
+    #     verif_code = self.request.data.get("verification_code")
+    #
+    #     if not verif_code:
+    #         raise ValidationError({"Указан неверный код проверки!"})
+    #
+    #     tg_client = TgClient(settings.BOT_TOKEN)
+    #     try:
+    #         tg_user = TgUser.objects.get(verification_code=verif_code)
+    #     except self.model.DoesNotExist:
+    #         raise ValidationError({"Не существует пользователя с таким кодом!"})
+    #
+    #     tg_user.user = self.request.user
+    #     tg_user.save()
+    #     tg_client.send_message(chat_id=tg_user.tg_chat_id, text=f"✅ Аккаунт подтвержден!!!")
+    #     return Response(data=verif_code, status=status.HTTP_201_CREATED)
